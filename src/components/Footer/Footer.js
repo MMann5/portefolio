d81fe55938dd3702136a4f836dc286b9ef5b941d
Michael Mann <87691866+MMann5@users.noreply.github.com>
2021-11-07 17:54:20 +0200

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://mmann5.github.io/portefolio/'
+      className='link footer__link'
+    >
+      Copyright © 2020 Michael Mann
+    </a>
+  </footer>
+)
+
+export default Footer
