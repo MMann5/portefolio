37f8a9ae94ef663330853affbc01b80d462713fa
Michael Mann <michael.mann55@gmail.com>
2021-11-07 17:00:02 +0200

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,119 @@
+const header = {
+  homepage: 'https://mmann5.github.io/portefolio/',
+  title: 'MM.',
+}
+
+const about = {
+  name: 'Michael Mann',
+  role: 'Full Stack/Frontend Web Developer',
+  description:
+    'With experience in writring single-page application using the latest WEB technonologies, including ReactJs, Redux, NodeJs and MongoDB. Graduate of the Coding Academy - an intensive coding bootcamp (640 hours)that qualifies Full Stack developers.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com/in/michael-mann-339545149 ',
+    github: 'https://github.com/MMann5',
+  },
+}
+
+const projects = [
+  {
+    name: 'Blogin',
+    description:
+      'Pixel perfect Landing Page exercise',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/MMann5/pixel-perfect',
+    livePreview: 'https://mmann5.github.io/pixel-perfect/',
+  },
+  {
+    name: 'Touch The Num',
+    description:
+      'You have to touch the numbers in the order as fast as possible',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/MMann5/touchthenum',
+    livePreview: 'https://mmann5.github.io/touchthenum/',
+  },
+  {
+    name: 'Pacman',
+    description:
+      'Pac-Man is an action maze chase video game; the player controls the eponymous character through an enclosed maze.',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/MMann5/pacman',
+    livePreview: 'https://mmann5.github.io/pacman/',
+  },
+  {
+    name: 'Ballboard',
+    description:
+      'Clone of Pacman with monster and balls',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/MMann5/ballboard',
+    livePreview: 'https://mmann5.github.io/ballboard/',
+  },
+  {
+    name: 'Meme Generator',
+    description:
+      'You can create and download your meme',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/MMann5/meme-generator',
+    livePreview: 'https://mmann5.github.io/meme-generator/',
+  },
+  {
+    name: 'Ballboard',
+    description:
+      'Clone of Pacman with monster and balls',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    sourceCode: 'https://github.com/MMann5/ballboard',
+    livePreview: 'https://mmann5.github.io/ballboard/',
+  },
+  {
+    name: 'Appsus',
+    description:
+      'Single Page Application of 3 applications : Mails, Notes and Bookstore',
+    stack: ['ReactJs', 'CSS', 'Javascript', 'API'],
+    sourceCode: 'https://github.com/MMann5/Appsus',
+    livePreview: 'https://mmann5.github.io/Appsus/',
+  },
+  {
+    name: 'Weather App',
+    description:
+      'First application with React Native',
+    stack: ['ReactNative', 'Style Component', 'Javascript', 'API', 'Expo'],
+    sourceCode: 'https://github.com/MMann5/meteo-app',
+    livePreview: 'https://github.com/MMann5/meteo-app',
+  },
+  {
+    name: 'Trellops',
+    description:
+      'Trello clone, Project End to End',
+    stack: ['ReactJs', 'Redux', 'Javascript', 'NodeJs', 'Express', 'MongoDB', 'React-Beautiful-DND', 'WebSockets'],
+    sourceCode: 'https://github.com/MMann5/trellops',
+    livePreview: 'http://trellops.herokuapp.com/#/',
+  },
+  {
+    name: 'Trellops',
+    description:
+      'Trello clone, Project End to End',
+    stack: ['ReactJs', 'Redux', 'Javascript', 'NodeJs', 'Express', 'MongoDB', 'React-Beautiful-DND', 'WebSockets'],
+    sourceCode: 'https://github.com/MMann5/trellops',
+    livePreview: 'http://trellops.herokuapp.com/#/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'ReactJs',
+  'React Native',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'NodeJs',
+  'MongoDB',
+]
+
+const contact = {
+  email: 'michael.mann55@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
